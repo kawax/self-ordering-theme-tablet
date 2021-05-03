92be1adfa0b31de4dbc40383e1420755aafe6a30
kawax <kawaxbiz@gmail.com>
2021-05-03 16:01:55 +0900

    header

--- resources/views/order/header.blade.php
+++ resources/views/order/header.blade.php
@@ -1,9 +1,12 @@
-
 <header class="p-3 text-white text-center font-bold bg-primary-500 flex justify-around">
-    <span class="text-2xl">{{ config('app.name', 'Laravel') }}</span>
+    <span class="text-2xl">
+        <a href="{{ route('order', ['table' => session('table')]) }}">
+        {{ config('app.name', 'Laravel') }}
+        </a>
+    </span>
     <span class="text-2xl">
         {{ __('テーブル : ') }}{{ session('table') }}
     </span>
-{{--    @include('ordering-tablet::order.info')--}}
+    {{--    @include('ordering-tablet::order.info')--}}
     @include('ordering-tablet::order.history')
 </header>
